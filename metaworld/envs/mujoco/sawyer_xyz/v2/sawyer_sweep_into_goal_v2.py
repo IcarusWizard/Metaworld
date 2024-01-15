700eb7f797ab33ce3e2042d2a5512988f4528261
Xingyuan Zhang <xingyuan.zhang@argmax.ai>
2024-01-15 23:22:07 +0100

    make sweep into resetable

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_sweep_into_goal_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_sweep_into_goal_v2.py
@@ -82,7 +82,7 @@ class SawyerSweepIntoGoalEnvV2(SawyerXYZEnv):
     def reset_model(self):
         self._reset_hand()
         self._target_pos = self.goal.copy()
-        self.obj_init_pos = self.get_body_com('obj')
+        self.obj_init_pos = self.init_config['obj_init_pos']
         self.obj_init_angle = self.init_config['obj_init_angle']
         self.objHeight = self.get_body_com('obj')[2]
 
@@ -92,6 +92,7 @@ class SawyerSweepIntoGoalEnvV2(SawyerXYZEnv):
                 goal_pos = self._get_state_rand_vec()
             self.obj_init_pos = np.concatenate((goal_pos[:2], [self.obj_init_pos[-1]]))
 
+        print(self.obj_init_pos)
         self._set_obj_xyz(self.obj_init_pos)
         self.maxPushDist = np.linalg.norm(self.obj_init_pos[:2] - np.array(self._target_pos)[:2])
 
